047a65ea487968f2e1c8edf621bd63327ee368f4
valachi <impulse221@yandex.ru>
2013-01-24 14:26:51 +0400

    add explaining comments

--- lib/Voyeur/audio_converters/ogg.rb
+++ lib/Voyeur/audio_converters/ogg.rb
@@ -5,7 +5,14 @@ module Voyeur
       "ogg"
     end
 
+    # т.к. экспериментальные кодеки не конвертили маленькие аудио файлы
+    # (меньше секунды в длину), то заменил на стандартный после экспериментов
+    # в консоли с чистым ffmpeg
+    # The encoder 'vorbis' is experimental but experimental codecs are
+    # not enabled, add '-strict -2' if you want to use it.
+    # Alternatively use the non experimental encoder 'libvorbis'.
     def convert_options
+      # "-strict -2 -acodec vorbis -aq 60"
       "-acodec libvorbis -aq 60"
     end
   end
